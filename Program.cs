d947c66b2cb64d67f8d21df3c70e805ad99aa8f8
Vadim <vadims.trojanovs@gmail.com>
2022-12-11 10:28:30 +0200

    1 и 2 задание

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int a;
+int b;
+Console.WriteLine("Введите число которое нужно возвести в степень");
+int.TryParse(Console.ReadLine(), out a);
+Console.WriteLine("Введите степень в которую нужно возвести число");
+int.TryParse(Console.ReadLine(), out b);
+
+int res= a;
+for (int i=1; i<b; i++)
+{
+res= res * a;
+}
+Console.WriteLine("A в степени B равно " + res);
